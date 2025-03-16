f6707d8e5a45dd16d33d040777c669b4f7cc4736
Gabriel Schneider <67759197+xinaiderf@users.noreply.github.com>
2025-03-16 20:42:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ async def process_videos(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8015)
